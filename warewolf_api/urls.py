59b9f5899ec61f632260043e340d53a4b4838548
Serial Lab <slab@serial-lab.com>
2020-10-27 12:41:35 -0600

    added default view to urls

--- warewolf_api/urls.py
+++ warewolf_api/urls.py
@@ -13,6 +13,11 @@ urlpatterns = [
         views.GetItemDetail.as_view(),
         name='item-detail'),
     url(
+        r'^decommission-parent/?$',
+        views.DeleteParentByChild.as_view(),
+        name='decommission-parent'
+    ),
+    url(
         #https://regex101.com/r/NQXjc1/1
         r'^decommission-parent/(?P<child_urn>(urn:epc:id:sscc:|urn:epc:id:sgtin:)([0-9]*).([0-9]*)?.([0-9\S]*))',
         views.DeleteParentByChild.as_view(),
